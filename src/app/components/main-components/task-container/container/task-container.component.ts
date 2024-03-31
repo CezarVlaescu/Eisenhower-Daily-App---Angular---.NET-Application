aaf5fe4c5f22c606897aabded954e9f17f5bf3cf
Vlaescu Cezar <108024461+CezarVlaescu@users.noreply.github.com>
2024-03-31 21:42:05 +0300

    Update task-container.component.ts

--- src/app/components/main-components/task-container/container/task-container.component.ts
+++ src/app/components/main-components/task-container/container/task-container.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { TaskSharedService } from '../services/task-shared-service.service';
+import { NewTask, DecidePool, DelegatePool, DoPool, DeletePool, Importance  } from '../types/taskType';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-task-container',
   templateUrl: './task-container.component.html',
   styleUrls: ['./task-container.component.scss']
 })
-export class TaskContainerComponent {
+export class TaskContainerComponent implements OnInit, OnDestroy {
+  
+  public tasks: NewTask[] = [];
+  public doTasks: DoPool = { tasks: []};
+  public delegateTasks: DelegatePool = { tasks: []};;
+  public decideTasks: DecidePool = { tasks: []};;
+  public deleteTasks: DeletePool = { tasks: []};;  
+  private $unsubscribe = new Subject<void>();
+
+  constructor(private taskSharedService: TaskSharedService) {}
+  
+  ngOnInit(): void {
+    this.taskSharedService.tasks$
+    .pipe(takeUntil(this.$unsubscribe))
+    .subscribe(tasks => {
+      this.tasks = tasks;
+      tasks.forEach(task => this.onTaskAdded(task));
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.$unsubscribe.next();
+    this.$unsubscribe.complete();
+  }
+
+  public onTaskAdded(newTask : NewTask) : void {    
+    if(this.tasks[0].importance === undefined){
+      throw new Error(`importance not set`)
+    }
+    switch(this.tasks[0].importance){
+      case Importance.Do : this.doTasks.tasks.push(newTask); break;
+      case Importance.Decide : this.decideTasks.tasks.push(newTask); break;
+      case Importance.Delegate: this.delegateTasks.tasks.push(newTask); break;
+      case Importance.Delete: this.delegateTasks.tasks.push(newTask); break;
+    }
+  }
 
 }
