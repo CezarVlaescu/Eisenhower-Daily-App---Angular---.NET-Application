73a7b542065f922a980b444b7de7cf88464d2306
Vlaescu Cezar <108024461+CezarVlaescu@users.noreply.github.com>
2024-04-03 20:43:56 +0300

    Update delegate-component.component.ts

--- src/app/components/main-components/task-container/components/delegate-component/delegate-component.component.ts
+++ src/app/components/main-components/task-container/components/delegate-component/delegate-component.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { DelegatePool, NewTask } from '../../types/taskType';
 
 @Component({
   selector: 'app-delegate-component',
   templateUrl: './delegate-component.component.html',
   styleUrls: ['./delegate-component.component.scss']
 })
-export class DelegateComponentComponent {
+export class DelegateComponentComponent implements OnInit {
+  
+  @Input() delegatePool: DelegatePool = { tasks: [] };
+  delegateTasks: NewTask[] = [];
 
+  ngOnInit(): void {
+    this.updateDelegateTask();
+  }
+  
+  private updateDelegateTask() : void {
+    this.delegateTasks = this.delegatePool.tasks;
+  }
 }
