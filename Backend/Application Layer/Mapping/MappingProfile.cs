c9dfe935b9f3d35ca3be5200333fc0dc53d44c0a
Vlaescu Cezar <108024461+CezarVlaescu@users.noreply.github.com>
2024-04-08 22:38:06 +0300

    Add files via upload

---
+++ Backend/Application Layer/Mapping/MappingProfile.cs
@@ -0,0 +1,22 @@
+﻿using Application_Layer.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application_Layer.Dtos;
+using AutoMapper;
+using Application_Layer.Dtos.Auth;
+using Core_Layer.Entities.Auth;
+
+namespace Application_Layer.Mapping
+{
+    // Automapper configuration profile
+    public class MappingProfile : Profile
+    {
+        public MappingProfile() 
+        {
+            CreateMap<RegisterDto, User>().ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.Username));
+        }
+    }
+}
