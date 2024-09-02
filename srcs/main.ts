d499203b0d2c2ef313845e4d32bb83aed451f925
Vlaescu Cezar <108024461+CezarVlaescu@users.noreply.github.com>
2024-09-02 13:20:36 +0300

    Delete srcs directory

--- srcs/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app-container/app.module';
-
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch((err: any) => console.error(err));
